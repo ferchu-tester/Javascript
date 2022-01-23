18f8b142d8a2211532f8bb4c809a7d92fadef2fc
ferchu-tester <ferchu.tester@gmail.com>
2022-01-23 20:56:29 -0300

    test

---
+++ curso javascript/5 - Condiciones o decisiones/Test del capitulo/1/js/app.js
@@ -0,0 +1,22 @@
+
+var a = 3;
+var b = 1;
+var c = 1;
+var opcion;
+
+opcion = c % b
+opcion += a;
+
+switch(opcion){
+    case 1: 
+    console.log("Opción 1");
+    break;
+    case 2: 
+    console.log("Opción 2");
+    break;
+    case 3: 
+    console.log("Opción 3");
+    break;
+    default: 
+    console.log("Nnguna opción");
+}
